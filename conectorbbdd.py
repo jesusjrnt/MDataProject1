e76a25f90202f0dc64d78c6582e7a4ae033f6132
jesusjrnt <145840818+jesusjrnt@users.noreply.github.com>
2023-12-12 21:46:51 +0100

    Update conectorbbdd.py

--- conectorbbdd.py
+++ conectorbbdd.py
@@ -6,10 +6,10 @@ import mysql.connector
 usuario = 'dataproject'  # El usuario predeterminado en XAMPP es 'root'
 contrasena = 'dataproject'  # Deja la contraseña en blanco si no la has configurado
 host = 'localhost'
-base_datos = 'dataproject'  # Reemplaza con el nombre de tu base de datos en phpMyAdmin
+base_datos = 'database'  # Reemplaza con el nombre de tu base de datos en phpMyAdmin
 
 # Crea la cadena de conexión a la base de datos usando sqlalchemy
-cadena_conexion = f"mysql+mysqlconnector://dataproject:dataproject@localhost/dataproject"
+cadena_conexion = f"mysql+mysqlconnector://dataproject:dataproject@localhost/database"
 
 # Crea el motor de la base de datos
 motor = create_engine(cadena_conexion)
